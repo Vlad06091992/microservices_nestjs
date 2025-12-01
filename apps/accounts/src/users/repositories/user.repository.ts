2571d639fb938112c31e2696c967dcc9003f22f1
Смирнов Владислав <vladislav.a.smirnov@gazprombank.ru>
2025-12-01 19:04:23 +0500

    10.4 - апдейт репозитория UsersRepository

--- apps/accounts/src/users/repositories/user.repository.ts
+++ apps/accounts/src/users/repositories/user.repository.ts
@@ -1,29 +1,51 @@
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from '../models/user.models';
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 import { Injectable } from '@nestjs/common';
 import { UserEntity } from '../entities/user.entity';
+import { ObjectId } from 'mongodb';
 
 @Injectable()
 export class UsersRepository {
-  constructor(@InjectModel(User.name) private readonly userModel:Model<User>) {
-  }
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<User>
+  ) {}
 
   //пример взаимодействия Entity и Model в mongoDB
   async createUser(user: UserEntity) {
-    const newUser = new this.userModel(user)
+    const newUser = new this.userModel(user);
     return await newUser.save();
   }
 
   async findUser(email: string) {
-    return this.userModel.findOne({email}).exec()
+    return this.userModel.findOne({ email }).exec();
+  }
+
+  async findUserById(id: string) {
+    const user = await this.userModel
+      .findOne({ _id: new ObjectId(id) })
+      // .select('') // альтернатива для выборки нужных полей, более правильная
+      .exec();
+
+    const {passwordHash, ...userData} = user
+
+    return {user:userData};
+
+  }
+
+  async findCoursesByUserId(id: string) {
+    const foundUser = await this.userModel
+      .findOne({ _id: new ObjectId(id) })
+      .exec();
+    return{ courses: foundUser.courses};
+
   }
 
   async deleteUser(email: string) {
-    return this.userModel.deleteOne({email}).exec()
+    return this.userModel.deleteOne({ email }).exec();
   }
 
   async findAllUsers() {
-    return this.userModel.find().exec()
+    return this.userModel.find().exec();
   }
 }
